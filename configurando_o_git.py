e0bf00e49fd31e2113397a8daded23acdb2bce7b
Ruubertdan123321 <prometheusamvs1@gmail.com>
2026-04-27 15:57:40 -0400

    Nova linha

--- configurando_o_git.py
+++ configurando_o_git.py
@@ -17,3 +17,4 @@
 ## 16# git branch -r (Vai mostrar a branch localmente, caso apareça vermelho quer dizer que está certo)
 ## 17# git push -u origin main (Vai publicar no site)
 ## 18# git checkout feature/pe-548 (git checkout vai mudar de branch exemplo: git checkout feature/pe-548 ; caso vc esteja na main vai para a feature/pe-548)
+## 19# git push --set-upstream origin NOMEDABRANCH (vai deixar a branch local)
